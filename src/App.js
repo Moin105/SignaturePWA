78b8d1b7ba82057b86b8e3ef18ba884fc2fd84d6
OSAMA SHIRAZ <osamashiraz60@gmail.com>
2023-01-09 20:07:15 +0500

    workimg

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 // import "https://unpkg.com/primereact/calendar/calendar.min.js";
 // import "https://unpkg.com/primereact/core/core.min.js"
-import { useState,useEffect } from 'react';
-import  SignaturePad from 'react-signature-canvas';
+import { useState, useEffect } from 'react';
+import SignaturePad from 'react-signature-canvas';
 import Logo from './Components/Logo/Logo';
 import Tab from './Components/Tab/Tab';
 import ItemDetails from './Pages/ItemDetails';
@@ -13,74 +13,74 @@ import Customer from './Pages/Customer';
 import InstallerDetails from './Pages/InstallerDetails';
 import ModalData from './Pages/ModalData';
 function App() {
-  const [tab ,setTab] = useState("details")
+  const [tab, setTab] = useState("details")
   // const [active ,setActive]= useState("1")
-  const [itemCreds ,setItemCreds]= useState(null)
+  const [itemCreds, setItemCreds] = useState(null)
   // const [active ,setActive]= useState(true)
-  const [itemDetails ,setItemDetails] = useState([])
-  const [customerDetails ,setCustomerDetails] = useState(null)
-  const [customerSignature ,setCustomerSignature] = useState(null)
-  const [installer,setInstaller] = useState(null)
-  const [insta,setInsta] = useState(null)
+  const [itemDetails, setItemDetails] = useState([])
+  const [customerDetails, setCustomerDetails] = useState(null)
+  const [customerSignature, setCustomerSignature] = useState(null)
+  const [installer, setInstaller] = useState(null)
+  const [insta, setInsta] = useState(null)
 
-  const [showModal ,setshowModal] =useState(false)
+  const [showModal, setshowModal] = useState(false)
   // const sendDataToParent = (index) => { // the callback. Use a better name
   //   console.log("onder",index);
   //   setTab(index);
   // };
   useEffect(() => {
-   gettemArr()
+    gettemArr()
   }, [insta])
-  
-  const gettemArr = (arrr)=>{
+
+  const gettemArr = (arrr) => {
     setInsta(arrr)
   }
-  const getItemsData = (arr,creds)=>{
+  const getItemsData = (arr, creds) => {
     setItemCreds(creds)
     setItemDetails(arr)
   }
-  const getModal = (x)=>{
+  const getModal = (x) => {
     setshowModal(x)
   }
-  const getCustomerData = (details , image) =>{
-  setCustomerDetails(details)
-  setCustomerSignature(image)
+  const getCustomerData = (details, image) => {
+    setCustomerDetails(details)
+    setCustomerSignature(image)
   }
-  const getInstallerData= (data)=>{
+  const getInstallerData = (data) => {
     setInstaller(data)
   }
   useEffect(() => {
-   console.log("active tab", tab)
-   console.log("otem",itemDetails)
+    console.log("active tab", tab)
+    console.log("otem", itemDetails)
   }, [itemDetails])
-  
-  console.log("const",tab)
+
+  console.log("const", tab)
   return (
-    <div className="App">
-   
-    <div className='container'>
-      <div className='upper'>
-        <Logo logo={logos}/>
-      </div>
-       
-       <div className='lower'>
-        <Tab setTab={setTab} tab={tab} />
-        <div className={tab == "details" ? "show" : "hide"}>
-        <ItemDetails gettemArr={gettemArr}  setTab={setTab} itemDetails={itemDetails} setItemDetails={getItemsData}/>
+    <div className="App" style={{ fontFamily: 'Poppins-Medium' }}>
+
+      <div className='container'>
+        <div className='upper'>
+          <Logo logo={logos} />
         </div>
-       {/* {tab == "details" &&  <ItemDetails setTab={setTab} itemDetails={itemDetails} setItemDetails={setItemDetails}/>} */}
-       <div className={tab == "customer" ? "show" : "hide"}>
-       <Customer setTab={setTab} getData={getCustomerData}/>
-       </div>
-       {/* {tab == "customer" && <Customer setTab={setTab}/>} */}
-       <div className={tab == "installer" ? "show" : "hide"}>
-       <InstallerDetails getInstallerData={getInstallerData} getModal={getModal}/>
-       </div>
-       {/* {tab == "installer" && <InstallerDetails/>} */}
-       </div>
-    </div>
-    <div>
-{  showModal &&    <ModalData itemDetails={itemDetails} itemCreds={itemCreds} customerSignature={customerSignature} customerDetails={customerDetails} showModal={showModal} getModal={getModal}  installer={installer}/> }    </div>
+
+        <div className='lower'>
+          <Tab setTab={setTab} tab={tab} />
+          <div className={tab == "details" ? "show" : "hide"}>
+            <ItemDetails gettemArr={gettemArr} setTab={setTab} itemDetails={itemDetails} setItemDetails={getItemsData} />
+          </div>
+          {/* {tab == "details" &&  <ItemDetails setTab={setTab} itemDetails={itemDetails} setItemDetails={setItemDetails}/>} */}
+          <div className={tab == "customer" ? "show" : "hide"}>
+            <Customer setTab={setTab} getData={getCustomerData} />
+          </div>
+          {/* {tab == "customer" && <Customer setTab={setTab}/>} */}
+          <div className={tab == "installer" ? "show" : "hide"}>
+            <InstallerDetails getInstallerData={getInstallerData} getModal={getModal} />
+          </div>
+          {/* {tab == "installer" && <InstallerDetails/>} */}
+        </div>
+      </div>
+      <div>
+        {showModal && <ModalData itemDetails={itemDetails} itemCreds={itemCreds} customerSignature={customerSignature} customerDetails={customerDetails} showModal={showModal} getModal={getModal} installer={installer} />}    </div>
     </div>
 
   );
--- src/Pages/ModalData.js
+++ src/Pages/ModalData.js
@@ -3,22 +3,17 @@ import { useEffect } from "react"
 import JsPDF from 'jspdf';
 import Sbutton from '../Components/Button/Sbutton'
 import './ModalData.css'
-
 function ModalData({ itemDetails, itemCreds, customerDetails, customerSignature, installer, getModal }) {
     const generatePDF = () => {
-
         const report = new JsPDF('portrait', 'pt', 'a4');
         report.html(document.querySelector('#report')).then(() => {
             console.log("randox", report)
             report.save('report.pdf');
-
         });
     }
-
     useEffect(() => {
         console.log("formfinaldata", itemDetails, itemCreds, customerDetails, customerSignature, installer)
     }, [customerSignature, installer])
-
     return (
         <><div className='overlay'>
             <div className='modal'>
@@ -37,44 +32,40 @@ function ModalData({ itemDetails, itemCreds, customerDetails, customerSignature,
                         <div className='bottom'>{itemDetails?.map((item, index) => {
                             return <div className='item-row' key={index}> <p className='p'>{item?.count}</p><p className='ps'>{item?.item}</p></div>
                         })}
-
                         </div>
                     </div>
+                    <div className='pdf-row'>
+                        <div className='left'><h5>Customer Name</h5></div>
+                        <div className='right'><p>{customerDetails?.customerName}</p></div>
+                    </div>
+                    <div className='pdf-row'>
+                        <div className='left'><h5>Email:</h5></div>
+                        <div className='right'><p>{customerDetails?.email}</p></div>
+                    </div>
+                    <div className='pdf-row'>
+                        <div className='left'><h5>Date:</h5></div>
+                        <div className='right'><p>{customerDetails?.date}</p></div>
+                    </div>
+                    <div className='pdf-box'>
+                        <div className='top'><h5>Items Details</h5></div>
+                        <div className='bottom'><img src={customerSignature} className='signatures' /></div>
+                    </div>
+                    <div className='pdf-row'>
+                        <div className='left'><h5>Installer Name:</h5></div>
+                        <div className='right'><p>{installer?.installerName}</p></div>
+                    </div>
+                    <div className='pdf-row'>
+                        <div className='left'><h5>Notes:</h5></div>
+                        <div className='right'><p>{installer?.notes}</p></div>
+                    </div>
                 </div>
-                <div className='pdf-row'>
-                    <div className='left'><h5>Customer Name</h5></div>
-                    <div className='right'><p>{customerDetails?.customerName}</p></div>
+                <div className='btn-row'>
+                    <Sbutton name="Close" type="close" func={getModal} onClick={() => { console.log("close") }} />
+                    <Sbutton name="Confirm" type="confirm" func={generatePDF} onClick={() => { console.log("close") }} />
                 </div>
-                <div className='pdf-row'>
-                    <div className='left'><h5>Email:</h5></div>
-                    <div className='right'><p>{customerDetails?.email}</p></div>
-                </div>
-                <div className='pdf-row'>
-                    <div className='left'><h5>Date:</h5></div>
-                    <div className='right'><p>{customerDetails?.date}</p></div>
-                </div>
-                <div className='pdf-box'>
-                    <div className='top'><h5>Items Details</h5></div>
-                    <div className='bottom'><img src={customerSignature} className='signatures' /></div>
-                </div>
-                <div className='pdf-row'>
-                    <div className='left'><h5>Installer Name:</h5></div>
-                    <div className='right'><p>{installer?.installerName}</p></div>
-                </div>
-                <div className='pdf-row'>
-                    <div className='left'><h5>Notes:</h5></div>
-                    <div className='right'><p>{installer?.notes}</p></div>
-                </div>
-            </div>
-            <div className='btn-row'>
-                <Sbutton name="Close" type="close" func={getModal} onClick={() => { console.log("close") }} />
-                <Sbutton name="Confirm" type="confirm" func={generatePDF} onClick={() => { console.log("close") }} />
             </div>
         </div>
-
-
         </>
     )
 }
-
 export default ModalData
